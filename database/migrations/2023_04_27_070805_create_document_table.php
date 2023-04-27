12555ea81ee9f3ad106a17903eab5b9012a52528
PpYCha <diasandiaz7@gmail.com>
2023-04-27 17:04:02 +0800

    Initial

---
+++ database/migrations/2023_04_27_070805_create_document_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('projectId')->unique();
+            $table->string('controlNumber')->uniqe();
+            $table->string('projectTitle')->require();
+            $table->string('contractor')->nullable();
+            $table->string('barangayDocument')->require();
+            $table->string('municipalityDocument')->require();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
